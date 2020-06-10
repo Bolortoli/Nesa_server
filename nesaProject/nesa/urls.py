11ac344c024959df7100e4ce8a623c3dff783284
Bolortoli <boogiibobko@gmail.com>
2020-06-10 16:53:28 +0800

    Added all routes and htmls

--- nesaProject/nesa/urls.py
+++ nesaProject/nesa/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('rewards/', views.rewards, name='rewards'),
+    path('contactus/', views.contactUs, name='contactUs'),
 ]
--- nesaProject/nesa/views.py
+++ nesaProject/nesa/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'home.html', {'name' : 'Bolortoli'})
+
+def rewards(request):
+    return render(request, 'rewards.html', {'name' : 'Bolortoli'})
+
+def contactUs(request):
+    return render(request, 'contactUs.html', {'name' : 'Bolortoli'})
