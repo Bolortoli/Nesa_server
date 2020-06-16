38e19b860e09738022429498b4dfdeb92e662cf1
Bolortoli <boogiibobko@gmail.com>
2020-06-16 14:16:56 +0800

    Removed duplicated directory

--- nesa_Project/nesa/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='home'),
-    path('rewards/', views.rewards, name='rewards'),
-    path('contactus/', views.contactUs, name='contactUs'),
-]
