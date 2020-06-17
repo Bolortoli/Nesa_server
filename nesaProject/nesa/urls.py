4acb217eb4b55c0bdc3f4ea9b085920643d269bc
Tuvshinbayar1234 <liltuku9911@gmail.com>
2020-06-18 00:43:11 +0800

    Filter Datepicker Added and footer Added and BlogGrid, BlogSingle

--- nesaProject/nesa/urls.py
+++ nesaProject/nesa/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('rewards/', views.rewards, name='rewards'),
     path('contactus/', views.contactUs, name='contactUs'),
+    path('blogGrid/', views.blogGrid, name='blogGrid'),
+    path('blogSingle/', views.blogSingle, name='blogSingle'),
+    
 ]
--- nesaProject/nesa/views.py
+++ nesaProject/nesa/views.py
@@ -3,10 +3,7 @@ from .models import ContactUs
 
 # Create your views here.
 def index(req):
-    return render(req, 'home.html')
-
-def rewards(req):
-    return render(req, 'rewards.html')
+    return render(req, 'home.html')               
 
 def contactUs(req):
     if req.method == 'POST':
@@ -15,3 +12,8 @@ def contactUs(req):
         item.save()
 
     return render(req, 'contactUs.html')
+def blogGrid(req):
+    return render(req, 'blogGrid.html')   
+
+def blogSingle(req):
+    return render(req, 'blogSingle1.html')
